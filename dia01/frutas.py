28dee3d80a8b065e101679ba0feb752eed1faa73
Leonardo Rocha <leorochaengenharia@gmail.com>
2025-04-15 10:06:55 -0300

    Início do curso de Machine Learning

---
+++ dia01/frutas.py
@@ -0,0 +1,41 @@
+# %%
+import pandas as pd
+
+df = pd.read_excel("../data/dados_frutas.xlsx")
+df
+
+# %%
+## Como aplicar o método do slide para descobrir a fruta?
+filtro_redonda = df['Arredondada'] == 1
+filtro_suculenta = df['Suculenta'] == 1
+filtro_vermelha = df['Vermelha'] == 1
+filtro_doce = df['Doce'] == 1
+df[filtro_redonda & filtro_suculenta & filtro_vermelha & filtro_doce]
+
+# %%
+## Como podemos fazer a máquina aprender?
+
+from sklearn import tree
+
+features = ['Arredondada', 'Suculenta', 'Vermelha', 'Doce']	
+target = 'Fruta'
+
+x = df[features]
+y = df[target]
+
+#%%
+
+arvore = tree.DecisionTreeClassifier(random_state=42)
+arvore.fit(x, y)
+
+#%%
+import matplotlib.pyplot as plt
+
+plt.figure(dpi=200)
+
+tree.plot_tree(arvore, 
+               class_names=arvore.classes_, 
+               feature_names=features, 
+               filled=True)
+
+plt.show()
