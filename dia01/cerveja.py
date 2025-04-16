e123ed5e6acef0ba9e5a1746aefab420db3e5a83
Leonardo Rocha <leorochaengenharia@gmail.com>
2025-04-16 09:45:58 -0300

    Árvore de decisão para os tipos de cerveja

--- dia01/cerveja.py
+++ dia01/cerveja.py
@@ -1,5 +1,7 @@
-# %%
+#%%
 import pandas as pd
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 df = pd.read_excel("../data/dados_cerveja.xlsx")
 df
@@ -7,43 +9,32 @@ df
 # %%
 ## Como podemos fazer a máquina aprender?
 
-features = ["temperatura","copo","espuma","cor"]
-target = "classe"
+features = ['temperatura', 'copo', 'espuma', 'cor']
+target = 'classe'
 
-X = df[features]
+x = df[features]
 y = df[target]
 
-# %%
+# transformação das variáveis em string para 0 e 1 (jeito didático)
+x = x.replace(
+    {
+        'mud': 1, 'pint': 0,
+        'sim': 1, 'não': 0,
+        'escura': 1, 'clara': 0,
+    }
+)
 
-X = X.replace({
-    "mud":1, "pint":0,
-    "sim":1, "não":0,
-    "escura":1, "clara":0,    
-})
-
-X
-
-# %%
-from sklearn import tree
-
+# random_state -> garante que todos vão ver a mesma árvore
 arvore = tree.DecisionTreeClassifier(random_state=42)
-arvore.fit(X, y)
+arvore.fit(x,y)
 
-# %%
+plt.figure(dpi=300)
+tree.plot_tree(arvore, class_names=arvore.classes_, feature_names=features, filled=True)
 
-import matplotlib.pyplot as plt
+plt.show()
 
-plt.figure(dpi=600)
+#%%
 
-tree.plot_tree(arvore,
-               class_names=arvore.classes_,
-               feature_names=features,
-               filled=True)
-
-# %%
-# "temperatura","copo","espuma","cor"
-
-probas = arvore.predict_proba([[-5, 1, 0, 1]])[0]
-
+# probalidades de ser: temperatura -1, copo mud, espuma não e cor escura
+probas = arvore.predict_proba([[-1, 1, 0, 1]])[0]
 pd.Series(probas, index=arvore.classes_)
-# %%
